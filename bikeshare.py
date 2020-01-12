7b9bbc58d85721a601d889ca37751afb32a9d5ca
Qifan Pan <chel.pan@hotmail.com>
2020-01-11 19:05:29 -0800

    add comment to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -16,6 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = None
     month = None
@@ -75,6 +76,7 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    # read csv files
     df = pd.read_csv(CITY_DATA[city])
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['month'] = df['Start Time'].dt.month
